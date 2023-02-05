07d8385ed274ab0d5197b1ce857a565d76dc8325
Mustafa Çağrı Güven <mustafacagri@users.noreply.github.com>
2023-02-05 23:37:13 +0300

    chore: activate the github action with the deployment.yml

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue3-pinia-typescript/',
   plugins: [vue()],
   resolve: {
     alias: {
